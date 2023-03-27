8c6d0ecc71eaf7856985915f8045dff34eed938a
GalalMohammed <galalmoh2001@gmail.com>
2023-03-27 21:32:32 +0200

    add main.h

--- main.h
+++ main.h
@@ -5,8 +5,10 @@
 #include <stdarg.h>
 
 int _putchar(char c);
+void _strrev(char *str);
+char *my_itoa(int i, char *strout, int base);
+int print_int(int arg, char *tmp);
 int printstr(char *str);
 int _printf(const char *format, ...);
 
-#endif /* ifndef MAIN_H */
-
+#endif /* ifndef MAIN_H*/
